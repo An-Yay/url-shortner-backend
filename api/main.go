36c9ada7c90956c8fcc42e2e888226ae0239a909
Ananay Gupta <ananaygupta23@gmail.com>
2024-06-18 23:32:39 +0530

    struct for request and fiber added

--- api/main.go
+++ api/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveUrl)
+	app.Post("/api/v1", routes.ShortenURL)
+}
+
+func main() {
+	err := godotevn.Load()
+	app := fiber.New()
+
+}
