709a4e019c193b6f30db90d214dde7e202a62dac
Ananay <ananaygupta23@gmail.com>
2024-06-19 00:45:53 +0530

    shortening url and sending the response

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
-	"github.com/asaskevich/govalidator"
-
+	"github.com/An-Yay/shorturl/database"
 	"github.com/An-Yay/shorturl/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -50,5 +54,81 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// Enforcing http
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	// Rate Limiting to 10 requests every 30 minutes
+	// checks if the user ip is already in the database
+	// if yes, reduce the allowed calls by 1, else add it to the database
+	// each ip expires within 30 minutes.
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	_, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 3600*time.Second).Err()
+	} else {
+		val, _ := r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate Limit Exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
+	var id string
+	// if no short url id is provided by the user
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ := r.Get(database.Ctx, id).Result()
+
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL alredy in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24 // hrs
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
+
+	// sending response
+	// includes - url, shorturl, expiry (hrs), calls remaining, ttl
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
+	r2.Decr(database.Ctx, c.IP())
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 
 }
